cdc52ddf000aacbab9773b5499c341884f6b5029
Maria Paderina <mariapaderina79@gmail.com>
2019-05-22 09:28:19 +0300

    Initial Commit

---
+++ Storytelling_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storytelling_test
+//
+//  Created by Maria Paderina on 5/22/19.
+//  Copyright © 2019 Maria Paderina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
